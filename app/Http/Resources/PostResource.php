4c7643b6e1a0bbaeb40294eaec0a7a6a9fa6fe4d
mairaouf6100 <mairaouf6100@gmail.com>
2021-10-25 12:02:00 -0700

    new

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,7 +15,7 @@ class PostResource extends JsonResource
     public function toArray($request)
     {
         return [
-            //'id'=>$this->id,
+             //'id'=>$this->id,
             'title'=>$this->title,
             'body'=>$this->body,
 
